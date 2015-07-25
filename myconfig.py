a4cdb1e9555934a5d380ccd849e3989cc392107a
a-outon <outon.x@gmail.com>
2015-07-25 15:16:22 -0500

    Create myconfig.py

---
+++ myconfig.py
@@ -0,0 +1,18 @@
+# myconfig.py
+
+MIN_SCORE = 80 # the default minimum score before it is downloaded
+
+a = 'artporn'
+b = 'cityporn'
+c = 'earthporn'
+d = 'foodporn'
+e = 'waterporn'
+f = 'imaginarylandscapes'
+g = 'movieposterporn'
+h = 'spaceporn'
+i = 'graffiti'
+
+subs = [a, b, c, d, e, f, g, h, i]
+n = 8
+#Remember to change 'n' to match the number of subreddits to use
+#I'm currently skipping the last subreddit on purpose
